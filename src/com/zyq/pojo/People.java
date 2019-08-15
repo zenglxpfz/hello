497a54970c91f3010f4b3288afa5fe955ebc8952
zyqzyq <zyqzyq@lxp.com>
2019-08-15 08:39:02 +0800

    第一次测试提交

---
+++ src/com/zyq/pojo/People.java
@@ -0,0 +1,13 @@
+package com.zyq.pojo;
+
+/**
+ * @author xmr
+ * @date 2019/4/23 16:25
+ * @description
+ */
+public class People {
+    private int age;
+    public static void main(String[] args) {
+
+    }
+}
